c93bceb7c5803bd2f465328c00c02faefa463e90
Arda Kizilenis <kizilenis@live.de>
2025-11-18 22:16:31 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express();
+const port = 1234;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        currentPage : "landing"
+    });
+})
+
+app.listen(port, () => {
+    console.log("Listening on port "+port);
+})
